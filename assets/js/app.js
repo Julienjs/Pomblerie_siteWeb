b87c2249b62dd2bfc412e55aef2a6d0950e84027
Rozelier Julien <rozelierjjs@gmail.com>
2021-11-29 16:00:12 +0100

    site web pomblerie

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+const ratio = .1
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+const handleIntersect = (entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('[class*="reveal-').forEach((r) => {
+    observer.observe(r)
+})
